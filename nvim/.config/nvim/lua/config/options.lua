6abca8a2a5fa43a908969880c18e5662916a3d8a
mo <moritz.fechte@gmail.com>
2025-04-09 16:57:42 +0200

    neovim changes finished typecrafts tutorial still need to add: tab/buffer line + keybinds unity stuff markdown mathjax stuff windows controlling stuff like opening a terminal inside and keybinds for switching between windows like neotree

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,5 +1,11 @@
-vim.opt.fillchars = {eob = " "}
+-- removes the tildes
+vim.opt.fillchars = { eob = " " }
 
+-- line numbering
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- allows ; to be used for command instead of just :
 vim.keymap.set("n", ";", ":", { noremap = true })
 
 vim.cmd("set expandtab")
@@ -7,4 +13,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
-
+vim.diagnostic.config({
+	virtual_text = true,
+	signs = true,
+	underline = true,
+	update_in_insert = true,
+})
